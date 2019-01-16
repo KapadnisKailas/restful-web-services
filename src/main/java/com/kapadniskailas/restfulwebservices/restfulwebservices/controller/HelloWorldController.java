af125cf625356c96047f34e857fcda7280bd93bb
KapadnisKailas <kapadnis.kailas@gmail.com>
2019-01-16 11:27:59 -0800

    added 1. swagger 2. spring boot actuator 3. static & dynamic filtering

--- src/main/java/com/kapadniskailas/restfulwebservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/kapadniskailas/restfulwebservices/restfulwebservices/controller/HelloWorldController.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,7 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.kapadniskailas.restfulwebservices.restfulwebservices.beans.HelloWorldBean;
+import com.kapadniskailas.restfulwebservices.restfulwebservices.beans.SomeBean;
 
 /**
  * @author kailaskapadnis
@@ -52,5 +57,22 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-internationalized1")
 	public String helloWorldInternationalized1() {
 		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+	
+	//static filtering on somebean class.
+	@GetMapping("/some-bean-static-filtering")
+	public SomeBean someBean() {
+		return new SomeBean("field1", "field2", "field3");
+	}
+	
+	//dynamic filtering on somebean class.
+	@GetMapping("/some-bean-dynamic-filtering")
+	public MappingJacksonValue retrieveSomeBean() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("someBeanFilter", filter);
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		return mapping;
 	}
 }
